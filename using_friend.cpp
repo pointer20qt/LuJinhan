317e3244a52f34f1e29bd03b58a75eaf350282ee
LuJinhanOne <2068591929@qq.com>
2020-02-13 15:10:08 +0800

    using和友元

---
+++ using_friend.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//using I = int;
+//
+//class student{
+//private:
+//	string sname;
+//	I age;
+//public:
+//	friend class man;
+//	student(string name,I age)
+//	{
+//		this->sname = name;
+//		this->age = age;
+//	}
+//};
+//
+//class man{
+//public:
+//	void getAge()
+//	{
+//		student s("Â³½ğìÊ",18);
+//		cout << s.age << endl;
+//	}
+//};
+//
+//
+//I main()
+//{
+//	man m;
+//	m.getAge();
+//}
